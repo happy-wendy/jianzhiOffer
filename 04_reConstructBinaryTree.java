d9176ce1b549439ec09f3f85bc1d6b2cd5200a16
longwenxiu <long_wenxiu@163.com>
2018-07-08 16:54:11 +0800

    递归的写法
    java里的切片是Arrays.copyOfRange(pre, 1, i+1)
    TreeNode node = new TreeNode(pre[0]);

--- 04_reConstructBinaryTree.java
+++ 04_reConstructBinaryTree.java
@@ -7,6 +7,22 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//同最后的python最简写法
+public class Solution {
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+       if(pre.length == 0||in.length == 0){
+            return null;
+        }
+        TreeNode node = new TreeNode(pre[0]);
+        for(int i = 0; i < in.length; i++){
+            if(pre[0] == in[i]){
+                node.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i+1), Arrays.copyOfRange(in, 0, i));
+                node.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i+1, pre.length), Arrays.copyOfRange(in, i+1,in.length));
+            }
+        }
+        return node;
+    }
+}
  //法一
 public class Solution {
     public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
