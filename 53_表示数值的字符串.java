34196294d5c98d5a409d6fb6c3f5be2f5deaf624
longwenxiu <long_wenxiu@163.com>
2018-08-28 12:18:51 +0800

    正则

--- 53_表示数值的字符串.java
+++ 53_表示数值的字符串.java
@@ -1,10 +1,20 @@
 //01正则表达式
 public boolean isNumeric_1(char[] str) {
-		String string = String.valueOf(str);
-		return string.matches("[\\+-]?[0-9]+(\\.[0-9]*)?([eE][\\+-]?[0-9]+)?");
+	String string = String.valueOf(str);
+	return string.matches("[+-]?[0-9]*(\\.[0-9]*)?([eE][+-]?[0-9]+)?");
+}
+//02
+//将char型数组转成字符串，再用Double的parseDouble方法转成double型数字，如果能转成功，则返回true。否则会发生NumberFormatException异常。返回false。
+public boolean isNumeric(char[] str) {
+	try {
+	    Double.parseDouble(new String(str));
+	} catch (NumberFormatException e) {
+	    return false;
 	}
+	return true;
+}
 
-//02一步步检验
+//03一步步检验
 public class Solution {
     public boolean isNumeric(char[] str) {
         if(str == null || str.length == 0){
@@ -66,12 +76,7 @@ public class Solution {
         while(index < str.length && str[index] >= '0' && str[index] <= '9'){
             index++;
         }
-        if(index == str.length){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (index == str.length) ? true : false;
     }
     private int scanDigits(char[] str, int index){
         while(index < str.length && str[index] >= '0' && str[index] <= '9'){
